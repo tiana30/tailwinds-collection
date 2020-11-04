9263bc9aad8bec10dc8b20265fb5d3c54cb29ee0
tiana30 <tiana3059@gmail.com>
2020-11-04 19:27:03 +0700

    Revert "add card"
    This reverts commit 73b90e03079706a7a8c1930d5cab3a49821bfb0c.

--- src/App.js
+++ src/App.js
@@ -2,38 +2,8 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-    <img src="https://source.unsplash.com/random" alt="random" className="w-full" />
-    <div className="px-6 py-4">
-      <div className="font-bold text-purple-500 text-xl mb-2">
-        Photo By Tia Nadya
-      </div>
-      <ul>
-        <li>
-          <strong>Views: </strong>
-          400
-        </li>
-
-        <li>
-          <strong>Downloads: </strong>
-          300
-        </li>
-
-        <li>
-          <strong>Likes: </strong>
-          700
-        </li>
-      </ul>
-    </div>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-        #tag1 
-      </span>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-        #tag2 
-      </span>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-        #tag3 
-      </span>
+    <div className="App">
+    <h1 className="text-blue-200">Hello World</h1>
     </div>
   );
 }
