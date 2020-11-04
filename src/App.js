54fc3e6b4ad5e7a30145e42570a7275f45593fc4
tiana30 <tiana3059@gmail.com>
2020-11-04 19:24:35 +0700

    create component image search

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import ImageCard from './components/ImageCard';
+import ImageSearch from './components/ImageSearch';
 
 function App() {
   const [images, setImages] = useState([]);
@@ -7,6 +8,7 @@ function App() {
   const [term, setTerm] = useState('');
 
   useEffect(() => {
+   setTimeout(() => {
     fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
     .then(res => res.json())
     .then(data => 
@@ -14,10 +16,14 @@ function App() {
       setIsLoading(false),
       )
     .catch(err => console.log("error" + err))
-  }, []);
+   }, 3000)
+  }, [term]);
 
   return (
     <div className="container mx-auto">
+      <ImageSearch searchText={(text) => setTerm(text)} />
+
+        
       {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading....</h1> 
       : 
       <div className="grid grid-cols-3 gap-4">
@@ -27,6 +33,9 @@ function App() {
         ))}
       </div>
       }
+
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1> }
+    
     </div>
   );
 }
