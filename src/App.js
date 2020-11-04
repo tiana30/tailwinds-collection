33f6d0cb96950b3c68ba1324d292e24002b2e7d5
tiana30 <tiana3059@gmail.com>
2020-11-04 11:33:04 +0700

    create component image card and get data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react';
+import ImageCard from './components/ImageCard';
 
 function App() {
   const [images, setImages] = useState([]);
@@ -16,38 +17,14 @@ function App() {
   }, []);
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-    <img src="https://source.unsplash.com/random" alt="random" className="w-full" />
-    <div className="px-6 py-4">
-      <div className="font-bold text-purple-500 text-xl mb-2">
-        Photo By Tia Nadya
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+        {images.map( thumbnail => (
+          <ImageCard key={thumbnail.id} image={thumbnail}/>
+        )
+
+        )}
       </div>
-      <ul>
-        <li>
-          <strong>Views: </strong>
-          400
-        </li>
-
-        <li>
-          <strong>Downloads: </strong>
-          300
-        </li>
-
-        <li>
-          <strong>Likes: </strong>
-          700
-        </li>
-      </ul>
-    </div>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-        #tag1 
-      </span>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-        #tag2 
-      </span>
-      <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-        #tag3 
-      </span>
     </div>
   );
 }
